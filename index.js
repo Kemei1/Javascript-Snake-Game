78fbd8962dde44791fbab6539eb0e403e479123c
Kemei Antony K <105278387+Kemei1@users.noreply.github.com>
2022-06-15 15:35:40 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,16 +4,3 @@ document.addEventListener("DOMContentLoaded", function () {
     newVal.innerHTML = '';
     pTag.appendChild(newVal);
   });
-  // Async Tests Example
-  /*
-  window.onModulesLoaded = new Promise( function( resolve, reject ) {
-    setTimeout(function() {
-      pTag = document.querySelector("div");
-      pTag.innerHTML = '';
-      newVal = document.createElement("p");
-      newVal.innerHTML = 'Hello World';
-      pTag.appendChild(newVal);
-      resolve();
-    }, 100)
-  });
-  */
